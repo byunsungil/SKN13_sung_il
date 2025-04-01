ca02293a095857e3ba6482b9dc52c51d00e8f113
sung_il <erety4100@naver.com>
2025-04-01 16:58:54 +0900

    0401

---
+++ mypackage/calculator.py
@@ -0,0 +1,20 @@
+#함수
+
+def plus(num1, num2):
+    return num1 + num2
+
+
+def minus(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def devide(num1, num2):
+    return num1 / num2
+
+if __name__ == "__main__":
+    print(plus(10,20))
+    print(">>>>>name<<<<<",__name__)
